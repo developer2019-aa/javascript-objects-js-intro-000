2aeed6688c4b55c8367bd09d63fdef6bc8610544
Allen <developer2019@icloud.com>
2020-05-08 03:48:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist, artist, song){
   playlist[artist] = song;
   return playlist
 }
+
+function removeFromPlaylist(playlist, name) {
+  delete playlist.name;
+  return playlist;
+}
